01ea4a1db2f0b770e75b4fe039636116b2d62714
Damon <crazygamerdl@hotmail.com>
2022-05-04 09:33:24 -0700

    Improved ts config, eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import { Server } from "socket.io";
+import { createServer } from "http";
 
-const io = new Server({});
+const httpServer = createServer();
+const io = new Server(httpServer, {
+    // Options
+});
+
+// We are not serving files to the client, so set the flag to false
+io.serveClient(false);
 
 io.on("connection", (socket) => {
-    
+    console.log("A user connected.");
 });
 
 io.listen(3000);
