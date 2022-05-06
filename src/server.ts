f94f536ad6c9783e213c21e1f0b33eee391b04c6
Damon <crazygamerdl@hotmail.com>
2022-05-06 10:18:36 -0700

    Setup more infrastructure

--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,23 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 
 const httpServer = createServer();
-const io = new Server(httpServer, { cors: { origin: "http://localhost:8080" } });
+const io = new Server(httpServer, { cors: {
+    origin: [
+        "localhost:8080",
+        /\.netlify\.app/
+    ]
+}});
+
+let usersConnected = 0;
 
 io.on("connection", (socket) => {
-    console.log("A user connected.");
-
-    socket.on("hello", (...args : any[]) => {
-        console.log("message received!");
-        io.emit("world");
-    });
+    usersConnected++;
+    console.log("A user connected. Active users: " + usersConnected);
 });
 
 io.on("disconnect", (reason) => {
-    console.log("A user disconnected.");
+    usersConnected--;
+    console.log("A user disconnected. Active users: " + usersConnected);
 });
 
 io.listen(3000);
