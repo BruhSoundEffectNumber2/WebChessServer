3ef1364a80f292a9ede957ec836cdc8da73d3cea
Damon <crazygamerdl@hotmail.com>
2022-05-06 09:40:16 -0700

    Add compatibility for replit

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,14 @@ const io = new Server(httpServer, { cors: { origin: "http://localhost:8080" } })
 io.on("connection", (socket) => {
     console.log("A user connected.");
 
-    socket.on("hello", (...args) => {
+    socket.on("hello", (...args : any[]) => {
         console.log("message received!");
+        io.emit("world");
     });
+});
+
+io.on("disconnect", (reason) => {
+    console.log("A user disconnected.");
 });
 
 io.listen(3000);
