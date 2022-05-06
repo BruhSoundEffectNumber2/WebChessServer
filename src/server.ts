544401bd276e322de037c430eb86398340d57dc5
Damon <crazygamerdl@hotmail.com>
2022-05-05 19:54:20 -0700

    Can connect to client on localhost

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import { Server } from "socket.io";
 import { createServer } from "http";
+import { Server } from "socket.io";
 
 const httpServer = createServer();
-const io = new Server(httpServer, {
-    // Options
-});
-
-// We are not serving files to the client, so set the flag to false
-io.serveClient(false);
+const io = new Server(httpServer, { cors: { origin: "http://localhost:8080" } });
 
 io.on("connection", (socket) => {
     console.log("A user connected.");
+
+    socket.on("hello", (...args) => {
+        console.log("message received!");
+    });
 });
 
 io.listen(3000);
+console.log("Listening");
