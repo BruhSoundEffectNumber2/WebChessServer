a6b87a0fac4a1e6a3e949eab88fbe6091c3fe807
Damon <crazygamerdl@hotmail.com>
2022-05-08 14:06:38 -0700

    Compatible with newest client

--- src/server.ts
+++ src/server.ts
@@ -4,22 +4,72 @@ import { Server } from "socket.io";
 const httpServer = createServer();
 const io = new Server(httpServer, { cors: {
     origin: [
-        "localhost:8080",
+        "http://localhost:8080",
         /\.netlify\.app/
     ]
 }});
 
-let usersConnected = 0;
+const matchingPlayers: string[] = [];
+const activeMatches: string[] = [];
 
 io.on("connection", (socket) => {
-    usersConnected++;
-    console.log("A user connected. Active users: " + usersConnected);
+    console.log("A user connected. Active users: " + io.engine.clientsCount);
 
-    io.on("disconnect", (reason) => {
-        usersConnected--;
-        console.log("A user disconnected. Active users: " + usersConnected);
+    socket.once("disconnect", (reason) => {
+        console.log("A user disconnected. Active users: " + io.engine.clientsCount);
+
+        matchingPlayers.splice(matchingPlayers.indexOf(socket.id), 1);
+    });
+
+    socket.on("move", (match, move) => {
+        socket.to(match).emit("move", move);
+    });
+
+    socket.on("match", () => {
+        matchingPlayers.push(socket.id);
+
+        tryToMatchPlayers();
+    });
+
+    socket.on("matchEnd", () => {
+        // End of match behavior, scoring, ranking, etc
     });
 });
+
+function tryToMatchPlayers(): void {
+    if (matchingPlayers.length < 2) {
+        console.log("Tried to match players, but there was nobody to match them with.");
+        return;
+    }
+
+    if (matchingPlayers.length == 2) {
+        matchPlayers(matchingPlayers[0], matchingPlayers[1]);
+        return;
+    }
+
+    // TODO: Matchmaking from a large pool of players
+}
+
+function matchPlayers(player1: string, player2: string): void {
+    console.log("Matching players " + player1 + " and " + player2);
+    
+    // Remove players from matching list
+    const player1Index = matchingPlayers.indexOf(player1);
+    matchingPlayers.splice(player1Index, 1);
+
+    const player2Index = matchingPlayers.indexOf(player2);
+    matchingPlayers.splice(player2Index, 1);
+
+    // Create a room for the players
+    const match: string = io.engine.generateId();
+    activeMatches.push(match);
+
+    io.to(player1).emit("matched", match, 1);
+    io.to(player2).emit("matched", match, 2);
+
+    io.to(player1).socketsJoin(match);
+    io.to(player2).socketsJoin(match);
+}
 
 io.listen(3000);
 console.log("Listening");
